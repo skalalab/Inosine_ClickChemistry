f38e0ee15d6489d609cd3ad33ade419b55b6076c
Contreras Guzman <econtrerasguzman@Morgridge.net>
2023-03-01 10:08:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import tifffile
 import czifile
 
 import numpy as np
+import re
 
 import cell_analysis_tools as cat
 from cell_analysis_tools.visualization import compare_images
@@ -15,21 +16,20 @@ from cell_analysis_tools.image_processing import kmeans_threshold
 #%%
 
 path_project = Path(r"Z:\0-Projects and Experiments\GG - ClickChemistry")
-
 list_czi_files = list(path_project.rglob("*.czi"))
 
 
-for path_czi in list_czi_files[:10]:
+for path_czi in list_czi_files[:]:
     pass
+    
+    base_name = path_czi.stem
+    list_folders_in_dir = [str(p) for p in path_czi.parent.glob("*") if p.is_dir()]
 
     im = czifile.imread(path_czi).squeeze()
     
     im_toxo_mask = im[0,...]
-    percentile_threshold = np.percentile(im_toxo_mask,99)
-    toxo_mask = im_toxo_mask > (im_toxo_mask.max()-3)
-
     toxo_mask = kmeans_threshold(im_toxo_mask, 
-                                 k=4, 
+                                 k=2,
                                  n_brightest_clusters=1)
     
     compare_images('original', im_toxo_mask, 
@@ -37,7 +37,7 @@ for path_czi in list_czi_files[:10]:
                    suptitle=f"{path_czi.name}")
     
     
-    # for data in im:
-    #     plt.title(f"{path_czi.name}")
-    #     plt.imshow(data)
-    #     plt.show()
+    path_output = list(filter(re.compile(f".*{base_name}.*").search, list_folders_in_dir))[0]
+    # save mask
+    
+    tifffile.imwrite(Path(path_output) / f"{base_name}_mask_toxo.tiff", toxo_mask)
